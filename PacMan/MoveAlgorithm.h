2701e7ec932c40a013c34ab925b2fead3c066a22
igorpieniek <pieniek97@gmail.com>
2021-06-01 20:12:22 +0200

    prepare abstract MoveAlgorithm class

---
+++ PacMan/MoveAlgorithm.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef __MOVEALGORITHM_H__
+#define __MOVEALGORITHM_H__
+
+#include "Position.h"
+#include "MapCell.h"
+#include "MapManager.h"
+
+
+class MoveAlgorithm{
+public:
+	virtual Position getNextPosition(Position& current) = 0;
+	virtual ~MoveAlgorithm();
+protected:
+
+	MoveAlgorithm(MapManager* manag) : mapManager(manag) {};
+
+private:
+	const MapManager* mapManager;
+};
+
+
+#endif //__MOVEALGORITHM_H__
