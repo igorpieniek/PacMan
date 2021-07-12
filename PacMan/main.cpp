1149f0d4057e9a5c2cc317e5c1127d6e3ee24a03
igorpieniek <pieniek97@gmail.com>
2021-07-12 19:24:24 +0200

    Add openGL main code

--- PacMan/main.cpp
+++ PacMan/main.cpp
@@ -121,11 +121,61 @@ static void app_thread(void) {
 
 int main() {
 
-	std::thread thInput(userInput_thread);
-	std::thread thApp(app_thread);
+	//std::thread thInput(userInput_thread);
+	//std::thread thApp(app_thread);
 
-	thInput.join();
-	thApp.join();
+	//thInput.join();
+	//thApp.join();
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "GLEW PROBLEM\n";
+	}
+
+
 	
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		
+
+		
+
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 
 }
