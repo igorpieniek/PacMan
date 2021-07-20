e8d920ee1879c268c15ee12a41c13676c9986fd6
igorpieniek <pieniek97@gmail.com>
2021-07-20 21:04:39 +0200

    Small main cleanup

--- PacMan/main.cpp
+++ PacMan/main.cpp
@@ -1,48 +1,24 @@
-
-#include <iostream>
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <algorithm>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <windows.h>
-
 #include "Position.h"
-#include "MapCell.h"
 #include "MapManager.h"
-#include "MoveAlgorithm.h"
-#include "RandMove.h"
-#include "Behaviour.h"
-
 #include "OponentManager.h"
 #include "Player.h"
-
-
 #include "GameRules.h"
 #include "PointsManager.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include "GraphicLayer/GraphicGLManager.h"
-
-
-
 #include "PlayerMovementManager.h"
 
 
-
-
 std::shared_ptr<Player> pl = std::make_shared<Player>(Position{ 1.0f,1.f }, 0.1f);
-
 PlayerMovementManager playerMovement(pl);
-
-void positionTest(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	playerMovement.keyActionCallback(window, key, scancode, action, mods);
-}
-
 
 
 int main() {
@@ -51,12 +27,11 @@ int main() {
 
 	std::shared_ptr<PointsManager> points = std::make_shared<PointsManager>( 10 );
 	std::shared_ptr<OponentManager> opManag = std::make_shared<OponentManager>( 4 );
+	
 
 	GameRules gameRules({ points.get(), opManag.get(), pl.get() });
 
 	
-	
-
 	GLFWwindow* window;
 
 	/* Initialize the library */
@@ -77,7 +52,9 @@ int main() {
 	}
 
 
-	glfwSetKeyCallback(window, positionTest);
+	//glfwSetKeyCallback(window, positionTest);
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+							{ playerMovement.keyActionCallback(window, key, scancode, action, mods); });
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
