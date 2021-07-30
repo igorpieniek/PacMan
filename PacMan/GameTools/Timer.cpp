8cd8f6c747c1d0d5a4b27ddcebf2f2f995940ab1
igorpieniek <pieniek97@gmail.com>
2021-07-30 17:03:43 +0200

    Add timer implementation

--- PacMan/GameTools/Timer.cpp
+++ PacMan/GameTools/Timer.cpp
@@ -4,3 +4,39 @@ Timer& Timer::instance(){
     static Timer tim;
     return tim;
 }
+
+void Timer::addPeriodElapsedCallback(std::function<void()> fun, double sec){
+    updateTime();
+    callbacks.push_back({ fun, sec, last, false });
+}
+
+Timer::Timer(){
+    updateTime();
+    thrd = std::thread(&Timer::process, this);
+    thrd.join();
+}
+
+void Timer::process(){
+    updateTime();
+    for (auto& cb : callbacks) {
+        std::chrono::duration<double> elapsed_seconds = last - cb.startTime;
+        if (elapsed_seconds.count() >= cb.requiredTime) {
+            cb.fun();
+            cb.isUsed = true;
+        }
+    }
+    deleteUsedCallbacks();
+
+}
+
+void Timer::deleteUsedCallbacks(){
+    auto itersToRemove = std::remove_if(callbacks.begin(),
+        callbacks.end(),
+        [](const CallbackElement& cb) { return cb.isUsed; });
+
+    callbacks.erase(itersToRemove, callbacks.end());
+}
+
+void Timer::updateTime(){
+    last = std::chrono::system_clock::now();
+}
